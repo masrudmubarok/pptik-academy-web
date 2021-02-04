e53b2e996526b31eb8086f2b1baba68d09dc8ded
MasrudMubarok <masrud.mubarok21@gmail.com>
2021-02-04 17:07:12 +0700

    Membuat View Halaman Students

--- application/views/siswa/list.php
+++ application/views/siswa/list.php
@@ -39,32 +39,32 @@
             <thead>
               <tr>
                 <th class="text-center" style="width: 20px">NO</th>
-                <th class="text-center">TANGGAL</th>
-                <th class="text-center">KATEGORI</th>
-                <th class="text-center">REKENING</th>
-                <th class="text-center" style="width: 250px">KETERANGAN</th>
-                <th class="text-center">PEMASUKAN</th>
-                <th class="text-center">PENGELUARAN</th>
-                <th class="text-center" style="width: 80px">Aksi</th>
+                <th class="text-center" style="width: 200px">NAMA SISWA</th>
+                <th class="text-center" style="width: 100px">USERNAME</th>
+                <th class="text-center" style="width: 100px">PASSWORD</th>
+                <th class="text-center" style="width: 150px">EMAIL</th>
+                <th class="text-center" style="width: 50px">KOTA</th>
+                <th class="text-center" style="width: 50px">NEGARA</th>
+                <th class="text-center" style="width: 80px">AKSI</th>
               </tr>
             </thead>
             <tbody>
               <?php 
               $i = 1; 
               $no =1;
-              foreach ($trs_list as $trs){ ?>
+              foreach ($swa_list as $swa){ ?>
                 <tr>
                   <td class="text-center" style="width: 20px"><?php echo $no++ ?></td>
-                  <td class="text-center"><?php echo $trs->tanggal_transaksi; ?></td>
-                  <td class="text-left"><?php echo $trs->nama_kategori; ?></td>
-                  <td class="text-left"><?php echo $trs->nama_pemilik; ?></td>
-                  <td class="text-left" style="width: 250px"><?php echo $trs->keterangan; ?></td>
-                  <td class="text-center">Rp <?php echo number_format($trs->pemasukan,0,',','.'); ?></td>
-                  <td class="text-center">Rp <?php echo number_format($trs->pengeluaran,0,',','.'); ?></td>
+                  <td class="text-center"><?php echo $swa->nama_siswa; ?></td>
+                  <td class="text-left"><?php echo $swa->username; ?></td>
+                  <td class="text-left"><?php echo $swa->password; ?></td>
+                  <td class="text-left" style="width: 250px"><?php echo $swa->email; ?></td>
+                  <td class="text-left"><?php echo $swa->kota; ?></td>
+                  <td class="text-left"><?php echo $swa->negara; ?></td>
                   <td class="text-center" style="width: 80px">
-                    <a href="<?= base_url('Transaksi/edit_trs/' . $trs->id_transaksi) ?>"><i class="fa fa-pencil text-secondary"></i></a>
-                    <a href="#" data-toggle="modal" data-target="#ModalDelete" data-id="<?php echo $trs->id_transaksi; ?>"
-                        data-title="<?php echo $trs->id_transaksi; ?>"><i class="fa fa-trash text-danger"></i></a>
+                    <a href="<?= base_url('Siswa/edit_swa/' . $swa->id_siswa) ?>"><i class="fa fa-pencil text-secondary"></i></a>
+                    <a href="#" data-toggle="modal" data-target="#ModalDelete" data-id="<?php echo $swa->id_siswa; ?>"
+                        data-title="<?php echo $swa->id_siswa; ?>"><i class="fa fa-trash text-danger"></i></a>
                   </td>
                 </tr>
               <?php $i++; } ?>
@@ -82,11 +82,11 @@
         <h5 class="modal-title">Konfirmasi Penghapusan</h5> <button type="button" class="close" data-dismiss="modal"> <span>×</span> </button>
       </div>
       <div class="modal-body">
-        <p>Anda yakin ingin menghapus data transaksi yang dipilih?</p>
+        <p>Anda yakin ingin menghapus data siswa yang dipilih?</p>
         <p id="mhstitle"></p>
       </div>
       <div class="modal-footer"> 
-        <a href="<?php echo base_url() ?>Transaksi/delete/<?php echo $trs->id_transaksi?>" class="btn btn-danger">Delete</a>
+        <a href="<?php echo base_url() ?>Siswa/delete/<?php echo $swa->id_siswa?>" class="btn btn-danger">Delete</a>
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> 
       </div>
     </div>
@@ -95,10 +95,10 @@
 <script>
 $('#ModalDelete').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget)
-    var trs_id = button.data('id')
+    var swa_id = button.data('id')
     var modal = $(this)
-    var trs_title = button.data('title');
-    document.getElementById('ktrtitle').innerHTML = rkn_title;
-    modal.find('.modal-footer a').attr("href", "<?= base_url() ?>Transaksi/delete/" + trs_id)
+    var swa_title = button.data('title');
+    document.getElementById('swatitle').innerHTML = swa_title;
+    modal.find('.modal-footer a').attr("href", "<?= base_url() ?>Siswa/delete/" + swa_id)
 })
 </script>
